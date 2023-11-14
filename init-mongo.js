dd4ce96551ae5120979f2ee70ffd8d8ee3d1a893
Maximesd17 <maximesd.kodoku17@gmail.com>
2023-11-14 15:52:59 +0100

    [ADD] backend containerisation with docker compose

---
+++ init-mongo.js
@@ -0,0 +1,16 @@
+console.log(`user: ${process.env.MONGO_INITDB_ROOT_USERNAME}`)
+console.log(`pws: ${process.env.MONGO_INITDB_ROOT_PASSWORD}`)
+console.log(`db: ${process.env.MONGO_INITDB_DATABASE}`)
+db.createCollection(process.env.MONGO_INITDB_DATABASE);
+db.createUser(
+    {
+        user: process.env.MONGO_INITDB_ROOT_USERNAME,
+        pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+        roles: [
+            {
+                role: "readWrite",
+                db: process.env.MONGO_INITDB_DATABASE
+            }
+        ]
+    }
+);
